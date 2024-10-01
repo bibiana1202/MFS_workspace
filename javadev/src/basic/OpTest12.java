2c7db2795f0b6feabfd3baca2cbb0d17501268cd
bibiana <105691874+bibiana1202@users.noreply.github.com>
2024-10-01 14:46:36 +0900

    타이틀
    커밋 내용

---
+++ javadev/src/basic/OpTest12.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class OpTest12 {
+	public static void main(String[] args) {
+		// 비트 연산자
+		int a = 10;
+		System.out.println(a >> 2);
+		System.out.println(a << 2);
+	}
+}

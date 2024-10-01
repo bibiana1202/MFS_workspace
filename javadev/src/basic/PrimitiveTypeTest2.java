132df4e2451a3b1a6dd9976661cb9ca9d951ee1d
bibiana <105691874+bibiana1202@users.noreply.github.com>
2024-10-01 17:47:17 +0900

    241001

---
+++ javadev/src/basic/PrimitiveTypeTest2.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class PrimitiveTypeTest2 {
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.toBinaryString(10));// 2진수로 바꿔줌
+		System.out.println(Integer.toOctalString(10));
+		System.out.println(Integer.toHexString(10));
+
+		
+		System.out.println(Integer.parseInt("a",16));
+		System.out.println(Integer.parseInt("z",36));
+//		System.out.println(Integer.parseInt("z",37)); // 에러
+		
+		
+		
+		
+		
+		
+	}
+}
